35b13dfefc34e82da931c7e40d6cbff7f4039645
Rachit Jain <rachitjain9596@gmail.com>
2023-09-16 00:41:02 +0530

    Get All Jobs

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ dotenv.config();
 import express from 'express';
 const app = express();
 import morgan from 'morgan';
+import { nanoid } from 'nanoid';
+
+let jobs = [
+  { id: nanoid(), company: 'apple', role: 'front-end' },
+  { id: nanoid(), company: 'google', role: 'back-end' },
+];
 
 if (process.env.NODE_ENV === 'development') {
   // logs the info about our requests
@@ -18,6 +24,10 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   console.log(req);
   res.json({ 'message': 'Data received', data: req.body });
+});
+
+app.get('/api/v1/jobs', (req, res) => {
+  res.status(200).json({ jobs });
 });
 
 const port = process.env.PORT || 5100;
