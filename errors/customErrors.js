aea99b09ae461eec61124368ff40b61df4a157fe
Rachit Jain <rachitjain9596@gmail.com>
2023-09-20 00:40:25 +0530

    Update customError

--- errors/customErrors.js
+++ errors/customErrors.js
@@ -1,10 +1,26 @@
 import { StatusCodes } from 'http-status-codes'; // Removing hard-coded status codes with refactored reusable status codes
 
-// NotFoundError is variable classname while Error is JavaScript inbuilt class
+// NotFoundError is variable class-name while Error is JavaScript inbuilt class
 export class NotFoundError extends Error {
   constructor(message) {
     super(message);
     this.name = 'NotFoundError';
     this.stack = StatusCodes.NOT_FOUND;
+  }
+}
+
+export class UnauthenticatedError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'UnauthenticatedError';
+    this.stack = StatusCodes.UNAUTHORIZED;
+  }
+}
+
+export class UnauthorizedError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'UnauthorizedError';
+    this.stack = StatusCodes.FORBIDDEN;
   }
 }
