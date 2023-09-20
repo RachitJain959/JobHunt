c34d68d2a8acce2e26827c6994781ac4d05892ad
Rachit Jain <rachitjain9596@gmail.com>
2023-09-20 21:43:46 +0530

    Add constants

--- models/jobModel.js
+++ models/jobModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { JOB_STATUS } from '../utils/constants.js';
 
 const JobSchema = new mongoose.Schema(
   {
@@ -6,8 +7,8 @@ const JobSchema = new mongoose.Schema(
     role: String,
     jobStatus: {
       type: String,
-      enum: ['pending', 'interview', 'declined'],
-      default: 'pending',
+      enum: Object.values(JOB_STATUS),
+      default: JOB_STATUS.PENDING,
     },
     jobType: {
       type: String,
