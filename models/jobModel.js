f21d8da5232216ed3acf8311753ec84740c82d2d
Rachit Jain <rachitjain9596@gmail.com>
2023-09-22 23:56:23 +0530

    Setup: Authenticate User p1

--- models/jobModel.js
+++ models/jobModel.js
@@ -19,8 +19,12 @@ const JobSchema = new mongoose.Schema(
       type: String,
       default: 'my-city',
     },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+    },
   },
   { timestamps: true }
 );
 
-export default mongoose.model('Job', JobSchema);
+export default mongoose.model('Job  ', JobSchema);
