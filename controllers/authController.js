a9cc04b5b754f7e7c14a50e0108fc07db0b1506f
Rachit Jain <rachitjain9596@gmail.com>
2023-09-22 17:19:58 +0530

    Hash User Password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,19 @@
 import User from '../models/userModel.js';
 import { StatusCodes } from 'http-status-codes';
+import bcrypt from 'bcryptjs';
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? 'admin' : 'user';
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPassword;
+
   const newUser = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ newUser });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ msg: 'user registered successfully!' });
 };
 
 export const login = async (req, res) => {
