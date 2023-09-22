76a29299168b77518a58a9fd51522a40ec172b62
Rachit Jain <rachitjain9596@gmail.com>
2023-09-22 10:55:51 +0530

    Create User

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,9 @@
 import User from '../models/userModel.js';
+import { StatusCodes } from 'http-status-codes';
 
 export const register = async (req, res) => {
-  res.send('register');
+  const newUser = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ newUser });
 };
 
 export const login = async (req, res) => {
