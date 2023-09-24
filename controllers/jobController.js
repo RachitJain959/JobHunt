5c08719d030a5a8d583c18246a10c518713cedfb
Rachit Jain <rachitjain9596@gmail.com>
2023-09-25 00:20:22 +0530

    Complete verify JWT

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -2,7 +2,7 @@ import Job from '../models/jobModel.js';
 import { StatusCodes } from 'http-status-codes'; // Removing hard-coded status codes with refactored reusable status codes
 
 export const getAllJobs = async (req, res) => {
-  //   console.log(req);
+  console.log(req.user);
   const jobs = await Job.find({});
   res.status(StatusCodes.OK).json({ jobs });
 };
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,18 @@
 import { UnauthenticatedError } from '../errors/customErrors.js';
+import { verifyJWT } from '../utils/tokenUtils.js';
 
 export const authenticateUser = async (req, res, next) => {
   const { token } = req.cookies;
   if (!token) throw new UnauthenticatedError('authentication invalid');
-  next();
+
+  // restricting the access to jobRoute. If the cookie is not present or JWT is invalid, access is restricted.
+  // If everything is fine, ie user is authenticated, this user obj will be available to all the controllers.
+  try {
+    const { userId, role } = verifyJWT(token);
+
+    req.user = { userId, role };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError('authentication invalid');
+  }
 };
