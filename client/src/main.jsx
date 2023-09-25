f03b57a29cdf89ad5a2717bbccfe381ed93421ea
Rachit Jain <rachitjain9596@gmail.com>
2023-09-26 02:49:59 +0530

    Complete Proxy Setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-fetch('http://localhost:5100/api/v1/test')
+fetch('/api/v1/test')
   .then((res) => res.json())
   .then((data) => console.log(data));
 
