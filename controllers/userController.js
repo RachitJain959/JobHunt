27c3a67d7bd632e6680b3a7f2b0d6676c40afa7d
Rachit Jain <rachitjain9596@gmail.com>
2023-09-25 22:01:51 +0530

    Complete get current user p1

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,14 +2,16 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/userModel.js';
 import Job from '../models/jobModel.js';
 
-export const getCurrentUser = (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: 'get current user' });
+export const getCurrentUser = async (req, res) => {
+  const user = await User.findOne({ _id: req.user.userId });
+
+  res.status(StatusCodes.OK).json({ user });
 };
 
-export const getApplicationStats = (req, res) => {
+export const getApplicationStats = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: 'application stats' });
 };
 
-export const updateUser = (req, res) => {
+export const updateUser = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: 'update user' });
 };
