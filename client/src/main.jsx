3a72647c081db96b584b398522415cc973f39af3
Rachit Jain <rachitjain9596@gmail.com>
2023-09-26 03:08:00 +0530

    Complete Axios Setup p2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import axios from 'axios';
+import customFetch from './utils/customFetch.js';
 
-const response = await axios.get('/api/v1/test');
+const response = await customFetch.get('/test');
 console.log(response);
-
-// fetch('/api/v1/test')
-//   .then((res) => res.json())
-//   .then((data) => console.log(data));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
