48ea3d8921b74947c470c3567b666b586e9d25a3
Rachit Jain <rachitjain9596@gmail.com>
2023-09-25 23:44:10 +0530

    Complete get application stats

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,5 +16,8 @@ export const updateUser = async (req, res) => {
 };
 
 export const getApplicationStats = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: 'application stats' });
+  const users = await User.countDocuments();
+  const jobs = await Job.countDocuments();
+
+  res.status(StatusCodes.OK).json({ users, jobs });
 };
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,6 @@
 import { UnauthenticatedError } from '../errors/customErrors.js';
 import { verifyJWT } from '../utils/tokenUtils.js';
+import { UnauthorizedError } from '../errors/customErrors.js';
 
 export const authenticateUser = (req, res, next) => {
   const { token } = req.cookies;
@@ -15,4 +16,13 @@ export const authenticateUser = (req, res, next) => {
   } catch (error) {
     throw new UnauthenticatedError('authentication invalid');
   }
+};
+
+export const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new UnauthorizedError('Unauthorized to access this route');
+    }
+    next();
+  };
 };
