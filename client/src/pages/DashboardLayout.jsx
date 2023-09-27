7ebadf0fea58a79fdbd5800c84517d79256e4897
Rachit Jain <rachitjain9596@gmail.com>
2023-09-28 00:35:38 +0530

    Complete User Logout

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,10 +1,11 @@
-import { Outlet, redirect, useLoaderData } from 'react-router-dom';
+import { Outlet, redirect, useLoaderData, useNavigate } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/Dashboard';
 import SmallSidebar from '../components/SmallSidebar';
 import BigSidebar from '../components/BigSidebar';
 import Navbar from '../components/Navbar';
 import { createContext, useContext, useState } from 'react';
 import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
 
 export const loader = async () => {
   try {
@@ -21,6 +22,7 @@ const DashboardLayout = ({ isDarkThemeEnabled }) => {
   const { user } = useLoaderData();
   const [showSidebar, setShowSidebar] = useState(false);
   const [isDarkTheme, setIsDarkTheme] = useState(isDarkThemeEnabled);
+  const navigate = useNavigate();
 
   const toggleSidebar = () => {
     setShowSidebar(!showSidebar);
@@ -34,7 +36,9 @@ const DashboardLayout = ({ isDarkThemeEnabled }) => {
   };
 
   const logoutUser = async () => {
-    console.log('Logout user');
+    navigate('/');
+    await customFetch.get('/auth/logout');
+    toast.success('Logged out successfully.');
   };
 
   return (
