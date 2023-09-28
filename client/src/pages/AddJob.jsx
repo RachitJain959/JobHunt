14fe86ac217781b01a51c319dca8abceadfb7c20
Rachit Jain <rachitjain9596@gmail.com>
2023-09-29 00:15:21 +0530

    Complete Add job structure

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,15 +1,32 @@
 import { Form, useNavigation, useOutletContext } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { FormRow } from '../components';
 
 const AddJob = () => {
   const { user } = useOutletContext();
-  const navigating = useNavigation();
-  const isSubmitting = navigating.state === 'submitting';
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
   return (
     <Wrapper>
       <Form method="post" className="form">
         <h4 className="form-title">add job</h4>
-        <div className="form-center"></div>
+        <div className="form-center">
+          <FormRow type="text" name="position" />
+          <FormRow type="text" name="company" />
+          <FormRow
+            type="text"
+            labelText="job location"
+            name="jobLocation"
+            defaultValue={user.location}
+          />
+          <button
+            type="submit"
+            className="btn btn-block form-btn"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'Submitting...' : 'Submit'}
+          </button>
+        </div>
       </Form>
     </Wrapper>
   );
