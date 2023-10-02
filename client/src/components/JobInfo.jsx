e6017885b361dbeb0300308fa788b64b972d6d8c
Rachit Jain <rachitjain9596@gmail.com>
2023-10-02 10:32:37 +0530

    Complete Job Component JobInfo

--- client/src/components/JobInfo.jsx
+++ client/src/components/JobInfo.jsx
@@ -1,5 +1,12 @@
-const JobInfo = () => {
-  return <h4>JobInfo</h4>;
+import Wrapper from '../assets/wrappers/JobInfo';
+
+const JobInfo = ({ icon, text }) => {
+  return (
+    <Wrapper>
+      <span className="job-icon">{icon}</span>
+      <span className="job-text">{text}</span>
+    </Wrapper>
+  );
 };
 
 export default JobInfo;
