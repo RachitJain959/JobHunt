428ec5b50921edf67c0a55a3d5b8056c7ebf2b6a
Rachit Jain <rachitjain9596@gmail.com>
2023-10-02 10:52:58 +0530

    Complete Route Params

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,8 +17,11 @@ import {
 import { action as registerAction } from './pages/Register';
 import { action as loginAction } from './pages/Login';
 import { action as addJobAction } from './pages/AddJob';
+import { action as editJobAction } from './pages/EditJob';
+
 import { loader as dashboardLoader } from './pages/DashboardLayout';
 import { loader as allJobsLoader } from './pages/AllJobs';
+import { loader as editJobLoader } from './pages/EditJob';
 
 const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
@@ -79,6 +82,8 @@ const router = createBrowserRouter([
           {
             path: 'edit-job/:id',
             element: <EditJob />,
+            loader: editJobLoader,
+            action: editJobAction,
           },
         ],
       },
--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,4 +1,17 @@
+import { useParams } from 'react-router-dom';
+
+export const loader = async ({ params }) => {
+  console.log(params);
+  return null;
+};
+
+export const action = async () => {
+  return null;
+};
+
 const EditJob = () => {
+  const params = useParams();
+  //   console.log(params);
   return <h1>EditJob</h1>;
 };
 
