2488f54965f9f53f2eb847ee0a547b871e337766
Rachit Jain <rachitjain9596@gmail.com>
2023-10-02 09:54:26 +0530

    Complete All Jobs Container

--- client/src/components/JobContainer.jsx
+++ client/src/components/JobContainer.jsx
@@ -1,9 +1,26 @@
 import Job from './Job';
 import Wrapper from '../assets/wrappers/JobsContainer';
-// import {useAllJobsContext} from '../'
+import { useAllJobsContext } from '../pages/AllJobs';
 
 const JobContainer = () => {
-  //   useAllJo;
+  const { data } = useAllJobsContext();
+  const { jobs } = data;
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <div className="jobs">
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
   return <h2>JobContainer</h2>;
 };
 
