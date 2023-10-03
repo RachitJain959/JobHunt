a66e3f70bc06f252d297f6b40a4b7d2f81a1efe2
Rachit Jain <rachitjain9596@gmail.com>
2023-10-03 15:43:46 +0530

    Admin Page Setup p1

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,5 +1,25 @@
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
+import { redirect, useLoaderData } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/StatsContainer';
+
+export const loader = async () => {
+  try {
+    const response = await customFetch.get('/users/admin/app-stats');
+    return response.data;
+  } catch (error) {
+    toast.error("You're not authorized to view this page.");
+    return redirect('/dashboard ');
+  }
+};
+
 const Admin = () => {
-  return <h1>Admin</h1>;
+  const { users, jobs } = useLoaderData();
+  return (
+    <Wrapper>
+      <h1>admin page</h1>
+    </Wrapper>
+  );
 };
 
 export default Admin;
