f155d4d1319d494f310a52045f42bbf4d05e151c
Rachit Jain <rachitjain9596@gmail.com>
2023-10-03 15:58:50 +0530

    Complete Admin Page

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -2,6 +2,8 @@ import { toast } from 'react-toastify';
 import customFetch from '../utils/customFetch';
 import { redirect, useLoaderData } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/StatsContainer';
+import { StatItem } from '../components/StatItem';
+import { FaCalendarCheck, FaSuitcaseRolling } from 'react-icons/fa';
 
 export const loader = async () => {
   try {
@@ -17,7 +19,20 @@ const Admin = () => {
   const { users, jobs } = useLoaderData();
   return (
     <Wrapper>
-      <h1>admin page</h1>
+      <StatItem
+        title="current users"
+        count={users}
+        color="#e9b949"
+        bcg="#fcefc7"
+        icon={<FaSuitcaseRolling />}
+      />
+      <StatItem
+        title="total jobs"
+        count={jobs}
+        color="#647acb"
+        bcg="#e0e8f9"
+        icon={<FaCalendarCheck />}
+      />
     </Wrapper>
   );
 };
