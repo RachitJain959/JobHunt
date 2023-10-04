ff21330355dbfc30fcd4de23cfc946ce90491be0
Rachit Jain <rachitjain9596@gmail.com>
2023-10-04 23:51:36 +0530

    Update user functionality p1

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/userModel.js';
 import Job from '../models/jobModel.js';
+import cloudinary from 'cloudinary';
+import { promises as fs } from 'fs';
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
