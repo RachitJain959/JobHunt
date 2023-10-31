20a45ef7a3c32d06aa262205d6681e78ca75f8f6
Rachit Jain <rachitjain9596@gmail.com>
2023-11-01 01:12:06 +0530

    Controlled inputs p1

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,38 +1,53 @@
 import Wrapper from '../assets/wrappers/DashboardFormPage';
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, useSubmit } from 'react-router-dom';
 import { FormRow, FormRowSelect, SubmitBtn } from '.';
 import { JOB_SORT_BY, JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
 
 const SearchContainer = () => {
+  const submit = useSubmit();
   return (
     <Wrapper>
       <Form className="form">
         <h5 className="form-title">Search form</h5>
         <div className="form-center">
-          <FormRow type="search" name="search" defaultValue="a" />
+          <FormRow
+            type="search"
+            name="search"
+            defaultValue="a"
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
+          />
 
           <FormRowSelect
             labelText="job status"
             name="jobStatus"
             list={['all', ...Object.values(JOB_STATUS)]}
             defaultValue="all"
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
           />
           <FormRowSelect
             labelText="job type"
             name="jobType"
             list={['all', ...Object.values(JOB_TYPE)]}
             defaultValue="all"
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
           />
           <FormRowSelect
             name="sort"
             defaultValue="newest"
             list={[...Object.values(JOB_SORT_BY)]}
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
           />
           <Link to="/dashboard/all-jobs" className="btn form-btn delete-btn">
             Reset Search Values
           </Link>
-          {/* TEMP!!! */}
-          <SubmitBtn formBtn />
         </div>
       </Form>
     </Wrapper>
