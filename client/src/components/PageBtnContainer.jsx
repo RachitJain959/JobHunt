90aac48fdf8f85a9f51b94f5445dcbcff93c7a23
Rachit Jain <rachitjain9596@gmail.com>
2023-11-04 00:39:40 +0530

    Render Buttons p1

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -1,11 +1,20 @@
+import Wrapper from '../assets/wrappers/PageBtnContainer';
 import { useAllJobsContext } from '../pages/AllJobs';
+import { HiChevronDoubleLeft } from 'react-icons/hi';
 
 const PageBtnContainer = () => {
   const {
     data: { noOfPages, currentPage },
   } = useAllJobsContext();
   console.log(noOfPages, currentPage);
-  return <h1>PageBtnContainer</h1>;
+  return (
+    <Wrapper>
+      <button className="btn prev-btn">
+        <HiChevronDoubleLeft />
+        prev
+      </button>
+    </Wrapper>
+  );
 };
 
 export default PageBtnContainer;
