c852e67da44c9ff447e2123bce4d328a9913177d
Rachit Jain <rachitjain9596@gmail.com>
2023-11-04 00:34:49 +0530

    Complete Pagination Setup

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -1,4 +1,10 @@
+import { useAllJobsContext } from '../pages/AllJobs';
+
 const PageBtnContainer = () => {
+  const {
+    data: { noOfPages, currentPage },
+  } = useAllJobsContext();
+  console.log(noOfPages, currentPage);
   return <h1>PageBtnContainer</h1>;
 };
 
